f9128ec97b02933f21b5ec52a2f244cae1c638dc
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2025-07-04 14:20:58 +0530

    password hash added

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,21 +2,28 @@ import express from 'express'
 import {z} from 'zod'
 import { UserSchema } from '../types/user'
 import { UserModel } from '../db'
+import bcrypt from "bcrypt"
+
+
 export const AuthRouter  =  express.Router()
+
 
 AuthRouter.post('/signup' ,async (req, res)=>{
 
         const Parse  = UserSchema.safeParse(req.body)
-        if(Parse.error){
+        if(!Parse.success){
+                console.error(Parse.error)
                 res.status(400).json({
                         err:"ivalid data format"
                 })
                 return
         }
+
         try{
+                const password = await  bcrypt.hash(process.env.BCRYPT_SECRET! ,  5)
                 const user  = await UserModel.create({
                         username:Parse.data.username,
-                        password:Parse.data.password // password should be hased
+                        password:password // password should be hased
                 })
                 res.json({
                         user
