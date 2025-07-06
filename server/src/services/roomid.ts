0e38e77f9e082c54bfc18677ddc4ef88e323be7b
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2025-07-06 17:04:38 +0530

    get rid of nanoid

--- server/src/services/roomid.ts
+++ server/src/services/roomid.ts
@@ -1,11 +1,11 @@
 import { customAlphabet } from "nanoid"
 
 export function GenerateRoomId() {
-	const generateId = customAlphabet(
-		"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
-		8
-	)
-	const roomId = generateId()
-        console.log(roomId)
-        return roomId
+	const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	let result = ""
+	const charactersLength = characters.length
+	for (let i = 0; i < 8; i++) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength))
+	}
+	return result
 }
