724d57ee0387e2f5920835d806aafdb81b86d328
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2025-07-08 17:02:28 +0530

    used Map for room management

--- Websocket-server/src/chat.ts
+++ Websocket-server/src/chat.ts
@@ -1,52 +1,35 @@
-
 import WebSocket, { WebSocketServer } from "ws"
 import jwt from "jsonwebtoken"
 const wss = new WebSocketServer({ port: 8080 })
 
-interface User{
-	name:string | any,
-	ws:WebSocket,
-	room:string
-}
-const Users : User[] = []
-
+const UserMap = new Map<string, Set<WebSocket>>()
 
 wss.on("connection", function connection(ws, request) {
 	console.log("user connected")
 
 	ws.on("message", async function message(data) {
 		const Message = JSON.parse(data.toString())
-		console.log(Message)
 		
-		// add user in rooms
-		if (Message.type == 'join_room'){
-			Users.push({
-				name:Message.data.sender,
-				ws:ws,
-				room:Message.data.room
-
-			})
-			console.log(Users)
-		}
-
-		// chating
-
-		
-		if (Message.type == 'chat'){
-			Users.forEach((user)=>{
-				
-				if (user.room === Message.data.room){
-					console.log(user)
-					user.ws.send(JSON.stringify(Message))
-				 }
+		if (Message.type == "join_room") {
+			// using map for room
+			if (!UserMap.get(Message.data.room)) {
+				UserMap.set(Message.data.room, new Set())
 			}
+			UserMap.get(Message.data.room)!.add(ws)
+			console.log(UserMap)
 
-			)
 		}
-		
-		
+
+		if (Message.type == "chat") {
+			const participants = UserMap.get(Message.data.room)
+
+			if(!participants) {
+				console.log("no participant")
+				return;
+			}
+			participants.forEach((socket) => {
+				socket.send(JSON.stringify(Message))
+			})
+		}
 	})
 })
-
-
-
