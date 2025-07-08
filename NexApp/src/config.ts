b2b90b13d5d2fee8a9dc8b846ea9bf7fdf32815c
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2025-07-08 14:31:55 +0530

    vercel fix

--- NexApp/src/config.ts
+++ NexApp/src/config.ts
@@ -1,3 +1,3 @@
-// export const BACKEND_URL = import.meta.env.VITE_API_URL
+export const BACKEND_URL = import.meta.env.VITE_API_URL
 
-export const BACKEND_URL = 'http://localhost:3001'
+// export const BACKEND_URL = 'http://localhost:3001'
